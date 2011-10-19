07703326e545896235f335fbd8665ac646783a7f
m <michal@duszyk.com>
2011-10-19 08:33:58 +0000

    error handling in thread classes

--- src/thread/ConditionVariable.cpp
+++ src/thread/ConditionVariable.cpp
@@ -1,21 +1,37 @@
 #include "thread/ConditionVariable.h"
+#include "thread/ThreadSyncException.h"
 
 ConditionVariable::ConditionVariable() {
-    pthread_cond_init(&cond, NULL);
+    int i;
+    if ((i = pthread_cond_init(&cond, NULL)) != 0) {
+        throw ThreadSyncException("Failed initializing condition variable, return value: " + i);
+    }
 }
 
 ConditionVariable::~ConditionVariable() {
-    pthread_cond_destroy(&cond);
+    int i;
+    if ((i = pthread_cond_destroy(&cond)) != 0) {
+        throw ThreadSyncException("Failed destroying condition variable, return value: " + i);
+    }
 }
 
 void ConditionVariable::signal() {
-    pthread_cond_signal(&cond);
+    int i;
+    if ((i = pthread_cond_signal(&cond)) != 0) {
+        throw ThreadSyncException("Failed signalling condition variable, return value: " + i);
+    }
 }
 
 void ConditionVariable::wait(Mutex& mtx) {
-    pthread_cond_wait(&cond, mtx.getPthreadPtr());
+    int i;
+    if ((i = pthread_cond_wait(&cond, mtx.getPthreadPtr())) != 0) {
+        throw ThreadSyncException("Failed waiting on condition variable, return value: " + i);
+    }
 }
 
 void ConditionVariable::wait(ScopeLock& lock) {
-    pthread_cond_wait(&cond, lock.getPthreadPtr());
+    int i;
+    if ((i = pthread_cond_wait(&cond, lock.getPthreadPtr())) != 0) {
+        throw ThreadSyncException("Failed waiting on condition variable, return value: " + i);
+    }
 }
--- src/thread/RwLock.cpp
+++ src/thread/RwLock.cpp
@@ -1,21 +1,37 @@
 #include "thread/RwLock.h"
+#include "thread/ThreadSyncException.h"
 
 RwLock::RwLock() {
-    pthread_rwlock_init(&lock, NULL);
+    int i;
+    if ((i = pthread_rwlock_init(&lock, NULL)) != 0) {
+        throw ThreadSyncException("Failed initializing rwlock, return value: " + i);
+    }
 }
 
 RwLock::~RwLock() {
-    pthread_rwlock_destroy(&lock);
+    int i;
+    if ((i = pthread_rwlock_destroy(&lock)) != 0) {
+        throw ThreadSyncException("Failed destroying rwlock, return value: " + i);
+    }
 }
 
 void RwLock::wrlock() {
-    pthread_rwlock_wrlock(&lock);
+    int i;
+    if ((i = pthread_rwlock_wrlock(&lock)) != 0) {
+        throw ThreadSyncException("Failed write locking rwlock, return value: " + i);
+    }
 }
 
 void RwLock::rdlock() {
-    pthread_rwlock_rdlock(&lock);
+    int i;
+    if ((i = pthread_rwlock_rdlock(&lock)) != 0) {
+        throw ThreadSyncException("Failed read locking rwlock, return value: " + i);
+    }
 }
 
 void RwLock::unlock() {
-    pthread_rwlock_unlock(&lock);
+    int i;
+    if ((i = pthread_rwlock_unlock(&lock)) != 0) {
+        throw ThreadSyncException("Failed unlocking rwlock, return value: " + i);
+    }
 }
